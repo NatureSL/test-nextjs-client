b4f07913f8309269ba4aabd36130f883f72260ff
NatureSL <varistrue@gmail.com>
2023-10-25 12:19:16 +0600

    feat: add ui kit

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import {
-  authControllerGetSessionInfo,
-  authControllerSignIn,
-} from "@/shared/api/generated";
+import { authControllerGetSessionInfo } from "@/shared/api/generated";
 import { useQuery } from "@tanstack/react-query";
+import { UiButton } from "@/shared/ui/ui-button";
+import { UiTextField } from "@/shared/ui/ui-text-filed";
+import { UiSelectField } from "@/shared/ui/ui-select-filed";
+import { UiLink } from "@/shared/ui/ui-link";
+import { UiSpinner } from "@/shared/ui/ui-spinner";
+import { UiPageSpinner } from "@/shared/ui/ui-page-spinner";
+import { UiHeader } from "@/shared/ui/ui-header";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export function HomePage() {
   const { data } = useQuery({
@@ -16,9 +17,31 @@ export function HomePage() {
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
-      {data?.email}
+      <UiHeader right={<div>{data?.email}</div>} />
+      <UiButton variant="primary">primary</UiButton>
+      <UiButton variant="secondary">primary</UiButton>
+      <UiButton variant="outlined">primary</UiButton>
+      <UiButton disabled variant="primary">
+        primary
+      </UiButton>
+      <UiTextField
+        label="Text field"
+        inputProps={{ placeholder: "Enter email..." }}
+      />
+      <UiTextField
+        error="Text field"
+        inputProps={{ placeholder: "Enter email..." }}
+      />
+      <UiTextField inputProps={{ placeholder: "Enter email..." }} />
+      <UiSelectField
+        selectProps={{ placeholder: "Enter email..." }}
+        options={[{ value: "1", label: "options" }]}
+      />
+      <UiLink href={"/"}>Text Link</UiLink>
+      <UiSpinner className="text-teal-600 w-20 h-20" />
+      {/* <UiPageSpinner /> */}
     </main>
   );
 }
