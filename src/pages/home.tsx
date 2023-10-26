29c969a69c0a46fd2e7e55731e3d3334385ed0ff
NatureSL <varistrue@gmail.com>
2023-10-26 22:42:20 +0600

    feat: implementing toggle-blocking & start block-items

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,49 +1,20 @@
-import { authControllerGetSessionInfo } from "@/shared/api/generated";
-import { useQuery } from "@tanstack/react-query";
-import { UiButton } from "@/shared/ui/ui-button";
-import { UiTextField } from "@/shared/ui/ui-text-filed";
-import { UiSelectField } from "@/shared/ui/ui-select-filed";
-import { UiLink } from "@/shared/ui/ui-link";
-import { UiSpinner } from "@/shared/ui/ui-spinner";
-import { UiPageSpinner } from "@/shared/ui/ui-page-spinner";
 import { UiHeader } from "@/shared/ui/ui-header";
-import { SignOutButton } from "@/features/auth/ui/sign-out-button";
-import { useSessionQuery } from "@/entities/session/queries";
+import { ToggleBlockingButton } from "@/features/auth/toggle-blocking/ui/toggle-blocking-button";
+import { Profile } from "@/widgets/profile";
 
 export function HomePage() {
-  const {data} = useSessionQuery()
   return (
-    <main className={`flex min-h-screen flex-col  justify-between `}>
-      <UiHeader
-        right={
-          <div>
-            {data?.email}
-            <SignOutButton></SignOutButton>
-          </div>
-        }
-      />
-      <UiButton variant="primary">primary</UiButton>
-      <UiButton variant="secondary">primary</UiButton>
-      <UiButton variant="outlined">primary</UiButton>
-      <UiButton disabled variant="primary">
-        primary
-      </UiButton>
-      <UiTextField
-        label="Text field"
-        inputProps={{ placeholder: "Enter email..." }}
-      />
-      <UiTextField
-        error="Text field"
-        inputProps={{ placeholder: "Enter email..." }}
-      />
-      <UiTextField inputProps={{ placeholder: "Enter email..." }} />
-      <UiSelectField
-        selectProps={{ placeholder: "Enter email..." }}
-        options={[{ value: "1", label: "options" }]}
-      />
-      <UiLink href={"/"}>Text Link</UiLink>
-      <UiSpinner className="text-teal-600 w-20 h-20" />
-      {/* <UiPageSpinner /> */}
-    </main>
+    <div className={`min-h-screen flex flex-col`}>
+      <UiHeader right={<Profile />} />
+      <div className="grid grid-cols-[200px_1fr]">
+        <aside className="px-5 pt-10">
+          <ToggleBlockingButton />
+        </aside>
+        <main className="pt-10 px-5">
+          <h1 className="text-2xl mb-8">Block list</h1>
+
+        </main>
+      </div>
+    </div>
   );
 }
